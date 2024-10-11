048e8ae580764c361e5c12fe79632aa738ad7ba0
Diogo Oliveira <dluks82@gmail.com>
2024-10-11 13:37:26 -0300

    :wrench: First commit

---
+++ src/main/java/dev/dluks/brasileirao/CodersBrasileiraoApplication.java
@@ -0,0 +1,13 @@
+package dev.dluks.brasileirao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodersBrasileiraoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodersBrasileiraoApplication.class, args);
+    }
+
+}
