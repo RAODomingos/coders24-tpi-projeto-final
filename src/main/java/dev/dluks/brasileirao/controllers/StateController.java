2fed8c0debb6b3dcf723e45cc2f534cf5ffe9e64
Diogo Oliveira <dluks82@gmail.com>
2024-10-12 18:29:31 -0300

    :recycle: Use new services

--- src/main/java/dev/dluks/brasileirao/controllers/StateController.java
+++ src/main/java/dev/dluks/brasileirao/controllers/StateController.java
@@ -1,23 +1,23 @@
 package dev.dluks.brasileirao.controllers;
 
 import dev.dluks.brasileirao.dtos.StateWithFewestGamesResponseDTO;
-import dev.dluks.brasileirao.services.MatchService;
+import dev.dluks.brasileirao.services.StatesWithFewestGamesBetweenYearsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/estatisticas/estado")
+@RequestMapping("/api/states/fewest-matches")
 public class StateController {
 
-    @GetMapping("/menos-jogos")
-    public StateWithFewestGamesResponseDTO getEstadoComMenosJogos(
-            @RequestParam(name = "anoInicio", defaultValue = "2003") String anoInicio,
-            @RequestParam(name = "anoFim", defaultValue = "2023") String anoFim
+    @GetMapping()
+    public StateWithFewestGamesResponseDTO getStatesWithFewestGamesBetweenYears(
+            @RequestParam(name = "anoInicio", defaultValue = "2003") String startYear,
+            @RequestParam(name = "anoFim", defaultValue = "2023") String endYear
     ) {
 
-        return MatchService.getStateWithFewestGamesBetweenYears(anoInicio, anoFim);
+        return StatesWithFewestGamesBetweenYearsService.execute(startYear, endYear);
 
     }
 
--- src/main/java/dev/dluks/brasileirao/controllers/TeamController.java
+++ src/main/java/dev/dluks/brasileirao/controllers/TeamController.java
@@ -1,21 +1,23 @@
 package dev.dluks.brasileirao.controllers;
 
-import dev.dluks.brasileirao.dtos.TeamWithMostWinsInResponseDTO;
-import dev.dluks.brasileirao.services.MatchService;
+import dev.dluks.brasileirao.dtos.TeamsWithMostWinsInResponseDTO;
+import dev.dluks.brasileirao.services.TeamsWithMostWinsInYearService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/estatisticas/vencedor")
+@RequestMapping("/api/teams/most-wins")
 public class TeamController {
 
-    @GetMapping("/{ano}")
-    public TeamWithMostWinsInResponseDTO getVencedorCampeonato(
-            @PathVariable(required = false) Integer ano
+    @GetMapping("/{year}")
+    public ResponseEntity<TeamsWithMostWinsInResponseDTO> getTeamsWithMostWinsInYear(
+            @PathVariable("year") int year
     ) {
-        return MatchService.getTeamsWithMostWinsInYear(ano);
+        TeamsWithMostWinsInResponseDTO result = TeamsWithMostWinsInYearService.execute(year);
+        return ResponseEntity.ok(result);
     }
 
 }
