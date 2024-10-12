11c134f7badb9ca44c76b3ab4ce51f1ec24cdb4d
Diogo Oliveira <dluks82@gmail.com>
2024-10-12 18:24:24 -0300

    :sparkles: Create `InvalidYearException`

---
+++ src/main/java/dev/dluks/brasileirao/exceptions/InvalidYearException.java
@@ -0,0 +1,9 @@
+package dev.dluks.brasileirao.exceptions;
+
+public class InvalidYearException extends RuntimeException {
+
+    public InvalidYearException(String message) {
+        super(message);
+    }
+
+}
