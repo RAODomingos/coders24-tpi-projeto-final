ba3f04a7dc6b05114410e16d6b2ade04abb07a4d
Diogo Oliveira <dluks82@gmail.com>
2024-10-15 15:23:21 -0300

    :sparkles: Add some example`s page using `Thymeleaf`

---
+++ src/main/java/dev/dluks/brasileirao/controllers/web/MathController.java
@@ -0,0 +1,36 @@
+package dev.dluks.brasileirao.controllers.web;
+
+import dev.dluks.brasileirao.dtos.team.TeamsWithMostWinsInResponseDTO;
+import dev.dluks.brasileirao.services.TeamsWithMostWinsInYearService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/most-wins")
+    public String showMostWinsForm(@RequestParam(value = "year", required = false) Integer year, Model model) {
+        if (year != null) {
+
+            TeamsWithMostWinsInResponseDTO result = TeamsWithMostWinsInYearService.execute(year);
+
+            if(result == null) {
+                model.addAttribute("error", "An error occurred while fetching the data");
+                return "most-wins-form";
+            }
+
+            List<Object[]> results = result.times().stream()
+                    .map(team -> new Object[]{team.nome(), team.vitorias()})
+                    .toList();
+
+            model.addAttribute("results", results);
+            model.addAttribute("selectedYear", year);
+        }
+        return "most-wins-form"; // Renderiza a mesma página com ou sem resultados
+    }
+
+}
