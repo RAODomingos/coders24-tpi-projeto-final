dc24758e7e2f32db0caddd67dd02fe379cfaf336
Romulo Domingos <romulo.domingos11@gmail.com>
2024-10-17 18:53:15 -0300

    Refactor: inclusion of endpoint for goal types

--- src/main/java/dev/dluks/brasileirao/controllers/GoalsController.java
+++ src/main/java/dev/dluks/brasileirao/controllers/GoalsController.java
@@ -1,18 +1,12 @@
 package dev.dluks.brasileirao.controllers;
 
 import dev.dluks.brasileirao.dtos.player.PlayersWithMostGoalsResponseDTO;
-import dev.dluks.brasileirao.dtos.team.TeamsWithMostWinsInResponseDTO;
-import dev.dluks.brasileirao.services.PlayersWithMostGoalsAgainstService;
 import dev.dluks.brasileirao.services.PlayersWithMostGoalsService;
-import dev.dluks.brasileirao.services.PlayersWithMostPenaltyGoalsService;
-import dev.dluks.brasileirao.services.TeamsWithMostWinsInYearService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/jogadores")
@@ -24,15 +18,10 @@ public class GoalsController {
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping("/gols-penalty")
-    public ResponseEntity<PlayersWithMostGoalsResponseDTO> getPlayersWithMostGoalsPenalty() {
-        PlayersWithMostGoalsResponseDTO result = PlayersWithMostPenaltyGoalsService.execute();
-        return ResponseEntity.ok(result);
-    }
 
-    @GetMapping("/gols-contra")
-    public ResponseEntity<PlayersWithMostGoalsResponseDTO> getPlayersWithMostGoalsAgainst() {
-        PlayersWithMostGoalsResponseDTO result = PlayersWithMostGoalsAgainstService.execute();
+    @GetMapping("/mais-gols/{typeGoals}")
+    public ResponseEntity<PlayersWithMostGoalsResponseDTO> getPlayersWithMostGoals(@PathVariable String typeGoals){
+        PlayersWithMostGoalsResponseDTO result = PlayersWithMostGoalsService.execute(typeGoals);
         return ResponseEntity.ok(result);
     }
 
